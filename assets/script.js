d36271497bad5761c5453370781fdd94b9afc079
denerbatista <denerbatista@icloud.com>
2023-09-25 21:49:46 -0300

    feat: initial

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+const display = document.querySelector("input");
+
+function digitar (valor) {
+    //display.value = display.value + valor
+    display.value += valor
+}
+
+function limpar () {
+    display.value = ""
+}
+
+function calcular () {
+    // eval() transforma string em algarismo javascript
+    display.value = eval(display.value)
+}
